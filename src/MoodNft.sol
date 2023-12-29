14d9fc1fa9747312b34ff5ab705a6daf90bd4222
Frankline Omondi <frankomosh197@gmail.com>
2023-12-29 10:30:06 +0300

    Mood changing Nft

---
+++ src/MoodNft.sol
@@ -0,0 +1,84 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract MoodNft is ERC721 {
+    //errors
+    error MoodNft__CantFlipMoodIfNotOwner();
+
+    uint256 private s_tokenCounter;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
+
+    enum Mood {
+        SAD,
+        HAPPY
+    }
+
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
+    ) ERC721("Mood NFT", "MN") {
+        s_tokenCounter = 0;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.SAD;
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        // Only the owner of the NFT can flip the mood
+        if (
+            getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender
+        ) {
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+
+        if (s_tokenIdToMood[tokenId] == Mood.SAD) {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        }
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        string memory imageURI;
+        if (s_tokenIdToMood[tokenId] == Mood.SAD) {
+            imageURI = s_sadSvgImageUri;
+        } else {
+            imageURI = s_happySvgImageUri;
+        }
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(),
+                                '", "description":"An NFT that reflects the owners mood.","attributes":[{"trait_type": "moodiness", "value":100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+        //{ "name": "Mood NFT" }
+    }
+}
---
+++ test/unit/DeployMoodNftTest.t.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MoodNft} from "../../src/MoodNft.sol";
+import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
+
+contract DeployMoodNftTest {
+    DeployMoodNft deployer;
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+    }
+
+    // function testConvertSvgToUri() public view {
+    //     string
+    //         memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjAwIDIwMCIgd2lkdGg9IjQwMCIgaGVpZ2h0PSI0MDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgICA8Y2lyY2xlIGN4PSIxMDAiIGN5PSIxMDAiIGZpbGw9InllbGxvdyIgcj0iNzgiIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iMyIgLz4KICAgIDxnIGNsYXNzPSJleWVzIj4KICAgICAgICA8Y2lyY2xlIGN4PSI2MSIgY3k9IjgyIiByPSIxMiIgLz4KICAgICAgICA8Y2lyY2xlIGN4PSIxMjciIGN5PSI4MiIgcj0iMTIiIC8+CiAgICA8L2c+CiAgICA8cGF0aCBkPSJtMTM2LjgxIDExNi41M2MuNjkgMjYuMTctNjQuMTEgNDItODEuNTItLjczIiBzdHlsZT0iZmlsbDpub25lOyBzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDM7IiAvPgo8L3N2Zz4=";
+    //     string
+    //         memory svg = "<svg viewBox='0 0 200 200' width='400' height='400' xmlns='http://www.w3.org/2000/svg'><circle cx='100' cy='100' fill='yellow' r='78' stroke='black' stroke-width='3'/></svg>";
+
+    //     string memory actualUri = deployer.svgToImageUri(svg);
+    //     assert(
+    //         keccak256(abi.encodePacked(actualUri)) ==
+    //             keccak256(abi.encodePacked(expectedUri))
+    //     );
+    // }
+}
